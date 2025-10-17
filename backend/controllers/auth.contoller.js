bb5874ab2bad860388a2731e2ac39a5a6a74a16b
hababisha <haileamlakderbie1@gmail.com>
2025-10-17 22:21:24 +0300

    refactor: student auth controller refactored

--- backend/controllers/auth.contoller.js
+++ backend/controllers/auth.contoller.js
@@ -1,57 +1,76 @@
 import { hashPassword, validatePassword } from "../utils/hash.js";
 import Students from "../models/student.model.js";
+import Admins from "../models/admin.model.js";
+import Wards from "../models/ward.model.js";
 import { generateAccessToken, generateRefreshToken } from "../services/generateToken.js";
 import  mongoose from "mongoose"
 
+/**
+ * Register Strudent
+ */
 export const registerStudent = async (req, res) =>{
     try {
-       const user = await Students.findOne({userId: req.body.studentId})
+        const { studentId, department, classYear, name, password } = req.body
 
-       if (user){
-        return res.status(400).json({message: "student already exists"})
-       }
+        const userExists = await Students.findOne({ studentId })
+        if (userExists){
+            return res.status(400).json({message: "student already exists"})
+        }
 
-       const hashedPass = await hashPassword(req.body.hashPassword)
+        const hashedPass= hashPassword(password)
 
-       const newUser = await Students.create({
-            studentId: req.body.studentId,
-            department: req.body.department,
-            classYear: req.body.classYear,
-            name: req.body.name,
+        const newUser = await Students.create({
+            studentId,
+            department,
+            classYear,
+            name,
+            password,
             password: hashedPass,
-       })
+            role: "student",
+        })
 
-       const accessToken = generateAccessToken(newUser.studentId)
-       res.cookie('accessToken', accessToken, {httpOnly: true})
-       res.status(201).json({
-        studentId: newUser.studentId
-       })
-    } catch (error) {
+        const accessToken = generateAccessToken(newUser._id, newUser.role) // _id - unique key gen by mongodb
+        const refreshToken = generateRefreshToken(newUser._id, newUser.role)
+        
+        res.cookie("accessToken", accessToken, {httpOnly: true})
+        res.status(201).json({
+            message: "student registered successfully",
+            studentId: newUser.studentId,
+        })
+
+    } catch(error){
         console.error(error)
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 }
 
 export const loginStudent = async (req, res) => {
     try {
-       const user = await Students.findOne({studentId: req.body.studentId}) 
-       if (!user){
-        return res.status(401).json({message: "Invalid student id or password"})
-       }
+        const { studentId, password } = req.body
 
-       const passwordVerification = await validatePassword(req.body.password)
-       if (!passwordVerification){
-        return res.status(401).json({message: "Invalid student id or password"})
-       }
+        const user = await Students.findOne({ studentId })
+        if (!user) {
+            return res.status(401).json({message: "invalid Student Id or password"})
+        }
 
-       const token = generateAccessToken(user.studentId)
-       res.cookie('accessToken', token, {httpOnly: true})
-       res.status(200).json({message: "login successful"})
+        const isValid= await validatePassword(password, user.password)
+        if (!isValid){
+            return res.status(401).json({ message: "invalid Student Id or password"})
+        }
+
+        const accessToken = generateAccessToken(user._id, user.role)
+        const refreshToken = generateRefreshToken(user._id, user.role)
+
+        res.cookie("accessToken", accessToken, {httpOnly: true})
+        res.status(200).json({
+            message: "Login successful"
+        })
     } catch (error) {
        console.error(error) 
        res.status(500).json({error: error.message})
     }
 }
+
 export const registerAdmin = async (req, res) => {
 
 }
